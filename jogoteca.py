043dab4990add57158757d001dea984b6a15cc61
Breno Baiardi <brenocbo@gmail.com>
2019-12-29 19:24:22 -0300

    problemas de rotas / / após url_for's

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = "key"
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -17,6 +18,8 @@ def index():
 
 @app.route("/novo")
 def novo():
+    if "usuario_logado" not in session or session["usuario_logado"] == None:
+        return redirect(url_for('login',proxima=url_for("novo")))
     return render_template("novo.html", titulo = "Novo Jogo")
 
 @app.route("/criar",methods=['POST'])
@@ -25,18 +28,28 @@ def criar():
     categoria = request.form["categoria"]
     console = request.form["console"]
     lista.append(Jogo(nome, categoria, console))
-    return redirect('/')
+    return redirect(url_for("index"))
 
 @app.route("/login")
 def login():
-    return render_template("login.html",titulo="login")
+    proxima = request.args.get("proxima")
+    return render_template("login.html", titulo="login",proxima=proxima)
 
 @app.route("/autenticar", methods=["POST"])
 def autenticar():
     if "mestra" == request.form["senha"]:
         session["usuario_logado"] = request.form["usuario"]
-        return redirect("/")
+        flash(request.form["usuario"]+" login realizado")
+        proxima_pagina = request.form["proxima"]
+        return redirect(proxima_pagina)
     else:
-        return redirect("/login")
+        flash("login não realizado")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session["usuario_logado"] = None
+    flash("Usuário deslogado")
+    return redirect(url_for("index"))
 
 app.run(debug=True)
